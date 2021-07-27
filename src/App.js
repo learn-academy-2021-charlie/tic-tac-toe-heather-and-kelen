fbf2d0e65216d802ef80c087e3067fd5192f05b9
hroulston <h_roulston@live.com>
2021-07-27 11:21:02 -0700

    create object to check for win conditions WIP

--- src/App.js
+++ src/App.js
@@ -8,17 +8,36 @@ class App extends Component{
     this.state = {
       squares: ['', '', '', '', '', '', '', '', ''],
       player: 'X'
+
     }
   }
 
   handleGameplay = (index) => {
+    const winCond = [
+      [0,1,2],
+      [0,3,6],
+      [0,4,8],
+      [1,4,7],
+      [2,5,8],
+      [2,4,6],
+      [3,4,5],
+      [6,7,8]
+    ]
     let { squares, player } = this.state
     if(!squares[index]){
       squares[index] = player
+      let playerMoves = squares.map((value, i)=> {
+        if(value === player){
+          return i
+        }
+      })
+      console.log(playerMoves)
       player === 'X' ? player = 'O' : player = 'X'
       this.setState({
         squares: squares,
         player: player
+
+
       })
     }
     console.log('parent')
@@ -30,7 +49,12 @@ class App extends Component{
         <h1>Tic Tac Toe</h1>
         <div id='gameboard'>
           {this.state.squares.map((value, index) => {
-            return <Square index={index} key={index} value={value} handleGameplay={this.handleGameplay}/>
+            return <Square
+            index={index}
+            key={index}
+            value={value}
+            handleGameplay={this.handleGameplay}
+            />
           })}
         </div>
       </>
