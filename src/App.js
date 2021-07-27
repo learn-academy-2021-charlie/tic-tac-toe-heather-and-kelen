592735439a6da3e824d9f68b8b19d019945b72a1
kel-man <kyyafuso@gmail.com>
2021-07-27 09:58:28 -0700

    adds gameboard with styles. players can alternate turns and place x or o in squares

--- src/App.js
+++ src/App.js
@@ -6,15 +6,33 @@ class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-      squares: [0, 0, 0, 0, 0, 0, 0, 0, 0]
+      squares: ['', '', '', '', '', '', '', '', ''],
+      player: 'X'
     }
+  }
+
+  handleGameplay = (index) => {
+    let { squares, player } = this.state
+    if(!squares[index]){
+      squares[index] = player
+      player === 'X' ? player = 'O' : player = 'X'
+      this.setState({
+        squares: squares,
+        player: player
+      })
+    }
+    console.log('parent')
   }
 
   render(){
     return(
       <>
         <h1>Tic Tac Toe</h1>
-        <Square />
+        <div id='gameboard'>
+          {this.state.squares.map((value, index) => {
+            return <Square index={index} key={index} value={value} handleGameplay={this.handleGameplay}/>
+          })}
+        </div>
       </>
     )
   }
